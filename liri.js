5518b6c4acf0a9c742102dc7fd4d5647ac5def43
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-02-28 22:26:19 -0600

    have the spotify and movie function working

--- liri.js
+++ liri.js
@@ -50,107 +50,101 @@ var getArtistNames = function (artist) {
 
 // Function for running a Spotify search
 var getMeSpotify = function (songName) {
-  spotify.search({
-    type: 'track',
-    query: songName,
-    limit: 1
-  },
-    function (error, songName){
-    if (error) {
-      songName = "What's my age again";
+  if (songName === undefined) {
+    songName = "What's my age again";
+  };
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
     }
-    console.log("\nTrack Info " + "\nArtist: " + songName.tracks.items[0].artist[0].name + "\nSong: " + data.tracks.items[0].name + "\nLink: " + songName.tracks.items[0].external_urls.spotify + "\nAlbum: " + songName.tracks.items[0].album.name + "\n" + "\nThis was a great search, let's look for another."))
+    console.log("\nThe information you seek:" + "\nArtist: " + data.tracks.items[0].artists[0].name + "\nSong: " + data.tracks.items[0].name + "\nLink: " + data.tracks.items[0].external_urls.spotify + "\nAlbum: " + data.tracks.items[0].album.name + "\n\n" + songName + " is a Great Song and should be a Top Hit")
+});
+}
+// Function for concert search
+var getMyBands = function (artist) {
+  console.log("\n ----- \n\nYou are searching for..." + artist + " 's next Show")
 
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+  axios.get(queryURL).then(
+    function (response) {
+      var jsonData = response.data;
 
-    spotify.search();
-  };
-
-  // Function for concert search
-  var getMyBands = function (artist) {
-    console.log("\n ----- \n\nYou are searching for..." + artist + " 's next Show")
-
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-    axios.get(queryURL).then(
-      function (response) {
-        var jsonData = response.data;
-
-        if (!jsonData.length) {
-          console.log("No results found for " + artist);
-          return;
-        }
-
-        var logData = [];
-
-        logData.push("Upcoming concerts for " + artist + "\nVenue: " + logData[0].venue.name + "\nLocation: " + logData[0].venue.contry + "\nDate of: " + logData[0].datetime + "\n Now go get them tickets!");
-
+      if (!jsonData.length) {
+        console.log("No results found for " + artist);
+        return;
       }
-    );
-  };
 
- 
-  // Function for running a Movie Search
-  var getMeMovie = function (movieName) {
-    if (movieName === undefined) {
-      movieName = "Mr Nobody";
+      var logData = [];
+
+      logData.push("Upcoming concerts for " + artist + "\nVenue: " + jsonData[0].venue.name + "\nLocation: " + jsonData[0].venue.contry + "\nDate of: " + jsonData[0].datetime + "\n Now go get them tickets!");
+
     }
+  );
+};
 
-    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(urlHit).then(
-      function (response) {
-        var jsonData = response.data;
-        console.log(jsonData);
-        console.log("\nMovie Info " +"\nTitle: " + jsonData.Title + "\nRelease Year: " + jsonData.Year + "\nRating: " + jsonData.Rated + "\nRelease Country: " + jsonData.Contry + "\nLanguage: " + jsonData.Language + "\nPlot: " + jsonData.Plot + "\nActors: " + jsonData.Actors + "\n" + "\n This is a Hit movie!");
-      }
-    );
-  };
+// Function for running a Movie Search
+var getMeMovie = function (movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
 
-  // Function for running a command based on text file
-  var doWhatItSays = function () {
-    fs.readFile("random.txt", "utf8", function (error, data) {
-      console.log(data);
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-      var dataArr = data.split(",");
-
-      if (dataArr.length === 2) {
-        pick(dataArr[0], dataArr[1]);
-      }
-      else if (dataArr.length === 1) {
-        pick(dataArr[0]);
-      }
-    });
-  };
-
-  // Function for determining which command is executed
-  var pick = function (command, commandData) {
-    // This will be the main function to control which method to call. See function "runThis" is calling this pick method
-    // useing the switch and case instead of if / else to get the correct action picked  where commandData will be what the user placed as a data to do
-    switch (command) {
-      case "concert-this":
-        getMyBands();
-        break;
-      case "spotify-this":
-        getMeSpotify();
-        break;
-      case "movie-this":
-        getMeMovie();
-        break;
-      case "do-what":
-        doWhatItSays(commandData);
-        break;
-      default:
-        console.log("I don't understand the words you are typing");
-        break;
+  axios.get(urlHit).then(
+    function (response) {
+      var jsonData = response.data;
+      console.log(jsonData);
+      console.log("\nMovie Info " + "\nTitle: " + jsonData.Title + "\nRelease Year: " + jsonData.Year + "\nRating: " + jsonData.Rated + "\nRelease Country: " + jsonData.Contry + "\nLanguage: " + jsonData.Language + "\nPlot: " + jsonData.Plot + "\nActors: " + jsonData.Actors + "\n" + "\n This is a Hit movie!");
     }
-  };
+  );
+};
 
-  // Function which takes in command line arguments and executes correct function accordingly
-  var runThis = function (argOne, argTwo) {
-    pick(argOne, argTwo);
-  };
+// Function for running a command based on text file
+var doWhatItSays = function () {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    console.log(data);
 
-  // MAIN PROCESS
-  // =====================================
-  runThis(process.argv[2], process.argv.slice(3).join(" "));
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    }
+    else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
+
+// Function for determining which command is executed
+var pick = function (command, commandData) {
+  // This will be the main function to control which method to call. See function "runThis" is calling this pick method
+  // useing the switch and case instead of if / else to get the correct action picked  where commandData will be what the user placed as a data to do
+  switch (command) {
+    case "concert-this":
+      getMyBands(commandData);
+      break;
+    case "spotify-this":
+      getMeSpotify(commandData);
+      break;
+    case "movie-this":
+      getMeMovie(commandData);
+      break;
+    case "do-what":
+      doWhatItSays(commandData);
+      break;
+    default:
+      console.log("I don't understand the words you are typing");
+      break;
+  }
+};
+
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function (argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv.slice(3).join(" "));
